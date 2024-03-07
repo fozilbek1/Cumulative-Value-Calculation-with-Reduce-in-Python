bc4deedea9beedbf55b97a3d3f66e72d249ed156
Fozil <91082400+fozilbek1@users.noreply.github.com>
2024-03-07 22:15:06 +0100

    Add files via upload
    # reduce() = apply a function to an iterable and reduce it to a single cumulative value.
    # performs function on first two elements and repeats process until 1 value remains
    
    # reduce(function, iterable)
    
    import functools
    
    # Example 1
    
    # letters = ["H", "E", "L", "L", "O"]
    # word = functools.reduce(lambda x, y,: x + y,letters)
    # print(word)
    
    # Example 2
    
    factorial = [5, 4, 3, 2, 1]
    result = functools.reduce(lambda x, y,: x * y,factorial)
    print(result)

---
+++ main.py
@@ -0,0 +1,18 @@
+# reduce() = apply a function to an iterable and reduce it to a single cumulative value.
+# performs function on first two elements and repeats process until 1 value remains
+
+# reduce(function, iterable)
+
+import functools
+
+# Example 1
+
+# letters = ["H", "E", "L", "L", "O"]
+# word = functools.reduce(lambda x, y,: x + y,letters)
+# print(word)
+
+# Example 2
+
+factorial = [5, 4, 3, 2, 1]
+result = functools.reduce(lambda x, y,: x * y,factorial)
+print(result)
